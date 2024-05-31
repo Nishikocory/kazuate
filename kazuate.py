59c09a88e18a6ab1fb87e642ab8416c48083c1d5
Nishikocory <1323502@ed.tus.ac.jp>
2024-05-31 16:30:05 +0900

    入力数字に対して大小判定

--- kazuate.py
+++ kazuate.py
@@ -1,2 +1,19 @@
+import random
 range = input("整数の範囲を設定してね\n")
-print(f"範囲は {range}  だね！ゲームスタート！")
+print(f"範囲は {range}  だね！ゲームスタート！\n数字を入力してね！")
+answer = random.randint(1, int(range))
+
+response = input()
+while (response != str(answer)):
+    if response > str(answer):
+        print("それより小さいよ")
+        response = input()
+    else:
+        print("それより大きいよ")
+
+
+
+
+ 
+   
+
