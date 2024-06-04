f8384ab4c57900e4adce4e30a5fa9b0a6f60740e
Nishikocory <1323502@ed.tus.ac.jp>
2024-06-04 21:21:22 +0900

    正解までの回答数を表示

--- kazuate.py
+++ kazuate.py
@@ -2,11 +2,19 @@ import random
 range = input("整数の範囲を設定してね\n")
 print(f"範囲は {range}  だね！ゲームスタート！\n数字を入力してね！")
 answer = random.randint(1, int(range))
-
 response = input()
-while (response != str(answer)):
-    if response > str(answer):
-        print("それより小さいよ")
-        response = input()
+count = 1
+while True:
+    if int(response) == answer:
+        print(f"正解！ {count} 回目で当たったね！")
+        break
     else:
-        print("それより大きいよ")
+        while int(response) != answer:
+            if int(response) > answer:
+                print("それより小さいよ")
+                response = input()
+                count += 1
+            else:
+                print("それより大きいよ")
+                response = input()
+                count += 1
